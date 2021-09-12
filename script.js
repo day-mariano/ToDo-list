4448dc24ddeb8522a7b99600f0c32388c13d7060
Dayana Mariano <dayanaraquelle@gmail.com>
2021-09-12 17:19:27 -0300

    Add layout

---
+++ script.js
@@ -0,0 +1,13 @@
+//Eventos
+function clicar() {
+    var buttonAddTask = document.getElementById('buttonAddTask');
+    buttonAddTask.style.background = 'rgb(126, 91, 255)';
+}
+function entrar() {
+    var buttonAddTask = document.getElementById('buttonAddTask');
+    buttonAddTask.style.background = 'rgb(177, 185, 247)';
+}
+function sair() {
+    var buttonAddTask = document.getElementById('buttonAddTask');
+    buttonAddTask.style.background = 'rgb(218, 218, 218)';
+}2
