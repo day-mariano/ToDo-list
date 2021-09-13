9e44d752d76680dfa87a3e89199f5701bc510e10
Dayana Mariano <dayanaraquelle@gmail.com>
2021-09-13 16:13:40 -0300

    Add tasks to the list

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-//Eventos
-function clicar() {
+//Eventos de mouse
+var buttonAddTask = document.getElementById('buttonAddTask');
+var newTask = document.getElementById('addTask');
+var list = document.getElementById('listWrapper');
+
+function clicarAdd() {
     var buttonAddTask = document.getElementById('buttonAddTask');
+    var newTask = document.getElementById('addTask');
+    var list = document.getElementById('listWrapper');
+
     buttonAddTask.style.background = 'rgb(126, 91, 255)';
+    list.innerHTML += `<li><label>
+    <input type="checkbox" name="inputTasks"> ${newTask.value}</label></li>`
+    newTask.value = ''
 }
-function entrar() {
+function entrarAdd() {
     var buttonAddTask = document.getElementById('buttonAddTask');
     buttonAddTask.style.background = 'rgb(177, 185, 247)';
 }
-function sair() {
+function sairAdd() {
     var buttonAddTask = document.getElementById('buttonAddTask');
     buttonAddTask.style.background = 'rgb(218, 218, 218)';
-}2
+}
+
+//Exibir lista de tarefas
+
+
+//Adicionar tarefa
+
+
+//Remover tarefa
+
+
+//Exibir lista de tarefas completadas
